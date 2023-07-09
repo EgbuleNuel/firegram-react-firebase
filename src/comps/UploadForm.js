28567a9b3e1f29be0e6030bd170635e3e2e2f44f
EgbuleNuel <egbulemmanuel@gmail.com>
2023-07-09 21:58:33 +0100

    started working on the progress bar

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -2,17 +2,17 @@ import React from "react";
 import { useState } from "react";
 
 const UploadForm = () => {
-  const [file, setfile] = useState(null);
+  const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
   // set file types allowed
   const types = ["image/png", "image/jpeg"];
   const changeHandler = (e) => {
     let selected = e.target.files[0];
     if (selected && types.includes(selected.type)) {
-      setfile(selected);
+      setFile(selected);
       setError("");
     } else {
-      setfile(null);
+      setFile(null);
       setError("please select an image file (png or jpeg");
     }
   };
@@ -27,6 +27,7 @@ const UploadForm = () => {
       <div className="output">
         {error && <div className="error">{error}</div>}
         {file && <div>{file.name}</div>}
+        {file && <ProgressBar file={file} setfile={setFile} />}
       </div>
     </form>
   );
